1a789e47d932b4fe814f3fde380b53a0de250f6b
laurentgautron <laurentgautron@MacBook-Air-de-laurentgautron.local>
2026-02-25 16:04:24 +0100

    add  cors for frontend

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import router from "./src/routes/joke.routes.js";
 import morgan from "morgan";
+import cors from "cors";
 
 const app = express();
 const port = 3000
 
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(cors());
 app.use('/api/v1/jokes', router);
 
 export default app;
