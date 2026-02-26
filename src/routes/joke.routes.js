c6610b6266e64d41d0cc9f4f7c92194e683d78c6
laurentgautron <laurentgautron@MacBook-Air-de-laurentgautron.local>
2026-02-26 16:35:32 +0100

    swagger docs

--- src/routes/joke.routes.js
+++ src/routes/joke.routes.js
@@ -14,8 +14,59 @@ const router = Router();
  */
 router.get('/', getAllJokes);
 
+/**
+ * @swagger
+ * /jokes/random:
+ *   get:
+ *     summary: Récupère une blague aléatoire
+ *     responses:
+ *       200:
+ *         description: Une blague aléatoire
+ */
 router.get('/random', getRandomJoke);
+
+/**
+ * @swagger
+ * /jokes/{id}:
+ *   get:
+ *     summary: Récupère une blague par son id
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: L'id de la blague
+ *     responses:
+ *       200:
+ *         description: Blague trouvée
+ *       404:
+ *         description: Blague non trouvée
+ */
 router.get('/:id', getOneJoke);
+
+/**
+ * @swagger
+ * /jokes:
+ *   post:
+ *     summary: Ajoute une nouvelle blague
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               question:
+ *                 type: string
+ *               answer:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Blague créée
+ *       400:
+ *         description: Données invalides
+ */
 router.post('/', addJoke);
 
 export default router;
